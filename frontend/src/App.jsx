3afb1ebda7ca306f51ef3a3d7bee4a5c88256fc1
moriartyjim1970 <neverdie1970@outlook.com>
2026-09-25 13:32:28 +0800

    集成性别选择弹窗到入口
    Added gender state management and modal for gender selection.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import TabBar from './components/TabBar'
+import GenderModal from './components/GenderModal'
 import Home from './pages/Home'
 import Wardrobe from './pages/Wardrobe'
 import Upload from './pages/Upload'
@@ -10,8 +12,16 @@ import Day from './pages/Day'
 import About from './pages/About'
 
 export default function App() {
+  const [gender, setGender] = useState(() => localStorage.getItem('rizhi_gender'))
+
+  const handleGenderSelect = (selectedGender) => {
+    setGender(selectedGender)
+    window.location.reload()
+  }
+
   return (
     <div className="app-shell">
+      <GenderModal onSelect={handleGenderSelect} />
       <div className="pb-24">
         <Routes>
           <Route path="/" element={<Home />} />
